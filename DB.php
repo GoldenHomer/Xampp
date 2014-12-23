6e6bdd969f31a8df9abc42a00d10e5d5af834201
Jose Contreras <jocontre@joselcontreras.com>
2014-12-22 20:22:53 -0600

    Messing with SQL DB with PHP

---
+++ DB.php
@@ -0,0 +1,26 @@
+<?php
+
+  $link = mysqli_connect("localhost", "cl54-firstdb-2oe", "9DuSsSk_4MjNFEk", "cl54-firstdb-2oe"); // connecting to localhost, username of DB, password, name of db in cpanel at ecowebhosting.co.uk. Don't forget to always use a secure password.
+
+  if (mysqli_connect_error()) {
+  	die("Could not connect to the date") // Recall die will kill the php script if called upon.
+  }
+
+  //$query = "INSERT INTO 'users' ('name', 'email','password') VALUES('Ian', 'ian@mail.com', 'haggis1234')"; //Should get an entry in my users DB with this info everytime I refresh page.
+
+  $query = "UPDATE 'users' SET 'email'='ian@hotmail.com' WHERE 'name'='Ian' LIMIT 1";
+
+  mysqli_query($link, $query);
+
+  $query = "SELECT * FROM users";
+
+  if ($result=mysqli_query($link, $query)) {
+
+  	$row = mysqli_fetch_array($result);
+
+  	print_r($row);
+  }
+  else {
+  	echo "It failed.";
+  }
+?>
